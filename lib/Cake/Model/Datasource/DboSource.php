a5299eb23dade7689476e3ee48bcd9bb63e3f732
Martin Seitz <martin.seitz@walls.io>
2025-10-17 18:09:46 +0200

    Make sure that errorInfo is an array

--- lib/Cake/Model/Datasource/DboSource.php
+++ lib/Cake/Model/Datasource/DboSource.php
@@ -513,7 +513,10 @@ class DboSource extends DataSource {
 			}
 			return $query;
 		} catch (PDOException $e) {
-			//this is adding a dynamic property querystring
+			if (!is_array($e->errorInfo)) {
+				$e->errorInfo = [];
+			}
+
 			if (isset($query->queryString)) {
 				$e->errorInfo["queryString"] = $query->queryString;
 			} else {
